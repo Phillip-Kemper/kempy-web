cfbe9e371997a27dba88f06f5e3899dd3d2efd7c
phillip-kemper <phillip-kemper@gmx.net>
2022-10-02 21:25:45 +0200

    fix linting again?

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { Meta } from "@/layouts/Meta";
-import { Main } from "@/templates/Main";
+import { Meta } from '@/layouts/Meta';
+// eslint-disable-next-line import/extensions
+import { Main } from '@/templates/Main';
 
 const Index = () => {
   return (
@@ -15,19 +16,23 @@ const Index = () => {
         Software Engineer looking for problems to solve.
       </h1>
 
-      <img src="/assets/images/Image.png" className="w-1/5" />
+      <img
+        src="/assets/images/Image.png"
+        className="w-1/5"
+        alt="Phillip Kemper"
+      />
 
       <h1 className="">
         Computer Science Student at Technical University Munich.
       </h1>
       <h1 className="">B. Sc. Computer Science at RWTH Aachen University.</h1>
 
-      <h1 className="text-2xl font-bold pt-8">Current Projects</h1>
+      <h1 className="pt-8 text-2xl font-bold">Current Projects</h1>
       <h2>Counting Game</h2>
       <h2>Notion Table Embedder</h2>
       <h2>Raycast Extension</h2>
 
-      <h1 className="text-2xl font-bold pt-8">Blog</h1>
+      <h1 className="pt-8 text-2xl font-bold">Blog</h1>
       <h2>to be continued...</h2>
     </Main>
   );
--- client/src/templates/Main.tsx
+++ client/src/templates/Main.tsx
@@ -1,38 +1,35 @@
-import type { ReactNode } from "react";
+import type { ReactNode } from 'react';
 
-import { AppConfig } from "@/utils/AppConfig";
+import { AppConfig } from '@/utils/AppConfig';
 
 type IMainProps = {
-    meta: ReactNode;
-    children: ReactNode;
+  meta: ReactNode;
+  children: ReactNode;
 };
 
 const Main = (props: IMainProps) => (
-    <div>
-        <div className="w-full px-1 text-white antialiased">
-            {props.meta}
+  <div>
+    <div className="w-full px-1 text-white antialiased">
+      {props.meta}
 
-            <div className="mx-auto max-w-screen-md">
-                <div className="border-b border-white">
-                    <div className="pt-16 pb-8">
-                        <div className="text-3xl font-bold text-white">
-                            {AppConfig.title}
-                        </div>
-                        <div className="text-2xl font-bold">
-                            {AppConfig.description}
-                        </div>
-                    </div>
-                </div>
-
-                <div className="content py-5 text-xl">{props.children}</div>
-
-                <div className="border-t border-white py-8 text-center text-sm">
-                    © Copyright {new Date().getFullYear()} {AppConfig.site_name}
-                    .
-                </div>
+      <div className="mx-auto max-w-screen-md">
+        <div className="border-b border-white">
+          <div className="pt-16 pb-8">
+            <div className="text-3xl font-bold text-white">
+              {AppConfig.title}
             </div>
+            <div className="text-2xl font-bold">{AppConfig.description}</div>
+          </div>
         </div>
+
+        <div className="content py-5 text-xl">{props.children}</div>
+
+        <div className="border-t border-white py-8 text-center text-sm">
+          © Copyright {new Date().getFullYear()} {AppConfig.site_name}.
+        </div>
+      </div>
     </div>
+  </div>
 );
 
 export { Main };

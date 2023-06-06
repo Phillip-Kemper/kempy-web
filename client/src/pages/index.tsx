1a535fae7052889efe9e2d960728fd0794cfb0a7
phillip-kemper <phillip-kemper@gmx.net>
2023-06-06 23:34:31 +0200

    reformat

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Meta } from "@/layouts/Meta";
 import { getSortedPostsData } from "@/lib/posts";
-import Date from "@/components/date"; 
+import Date from "@/components/date";
 // eslint-disable-next-line import/extensions
 import { Main } from "@/templates/Main";
 
@@ -13,28 +13,19 @@ export async function getStaticProps() {
   };
 }
 
-
-const Index = ({allPostsData}) => {
+const Index = ({ allPostsData }) => {
   return (
     <Main meta={<Meta title="Phillip Kemper" description="Phillip Kemper." />}>
       <h1 className="text-2xl font-bold">📚 Blog.</h1>
-      <a href="https://twitter.com/Kemperino_" className="text-white font-bold">
-        <h2 className="text-lg ">
-          A Primer on Data Availability For ZK Rollups
-        </h2>
-        <h2 className="text-sm ">June 2023</h2>
-      </a>
 
-          {allPostsData.map(({ date, title, id}) => (
-      <a href={"/posts/"+id} className="pt-4 text-white font-bold">
-      <h2 className="text-lg ">
-        {title}
-      </h2>
-      <h2 className="text-sm ">
-              <Date dateString={date} />
-      </h2>
-    </a>
-          ))}
+      {allPostsData.map(({ date, title, id }) => (
+        <a href={"/posts/" + id} className="pt-4 text-white font-bold">
+          <h2 className="text-lg ">{title}</h2>
+          <h2 className="text-sm ">
+            <Date dateString={date} />
+          </h2>
+        </a>
+      ))}
 
       <h1 className="pt-8 text-2xl font-bold">🎓 Education.</h1>
       <h2>M. Sc. Informatics Candidate at Technical University Munich.</h2>
